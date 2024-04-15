cb41fae8adc6a462542cade5b10f880c0dcfa212
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2024-04-15 12:39:10 +0100

    Refactor Branch interface and its dependencies

--- src/number/relational/branch/Branch.ts
+++ src/number/relational/branch/Branch.ts
@@ -3,18 +3,21 @@ import { Fn, Math } from "@ibnlanre/types";
 export type Branch<
   Comparison extends Math.ComparisonResult,
   Positive extends number,
+  Zero extends number,
   Negative extends number
-> = Math.Branch<Comparison, Positive, Negative>;
+> = Math.Branch<Comparison, Positive, Zero, Negative>;
 
 export interface TBranch<
-  Positive extends number | void = void,
-  Negative extends number | void = void,
-  Comparison extends number | void = void
+  Positive extends number | void = never,
+  Zero extends number | void = never,
+  Negative extends number | void = never,
+  Comparison extends Math.ComparisonResult | void = void
 > extends Fn<{
     0: number;
     1: number;
     2: number;
+    3: Math.ComparisonResult;
   }> {
-  slot: [Positive, Negative, Comparison];
-  data: Branch<this[2], this[0], this[1]>;
+  slot: [Positive, Zero, Negative, Comparison];
+  data: Branch<this[3], this[0], this[1], this[2]>;
 }
