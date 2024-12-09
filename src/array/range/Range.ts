47014ee864b4512cb4e80f8afdd0ca44979a85de
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2024-12-09 04:21:57 +0100

    feat: Refactor Range type and introduce TRange interface for enhanced range handling

--- src/array/range/Range.ts
+++ src/array/range/Range.ts
@@ -3,6 +3,7 @@ import type {
   Ceil,
   Concat,
   Divide,
+  Fn,
   InferArray,
   InferNumber,
   Length,
@@ -79,9 +80,20 @@ type FlatChunk<
   ? FlatChunk<Rest, Concat<Result, Head>>
   : Result;
 
-export type Range<Start extends number, End extends number> = GenerateChunks<
-  Start,
-  End
+export type Range<From extends number, To extends number> = GenerateChunks<
+  From,
+  To
 > extends InferArray<infer Chunk, number[]>
   ? FlatChunk<Chunk>
   : never;
+
+export interface TRange<
+  From extends number | void = void,
+  To extends number | void = void
+> extends Fn<{
+    0: number;
+    1: number;
+  }> {
+  slot: [From, To];
+  data: Range<this[0], this[1]>;
+}
